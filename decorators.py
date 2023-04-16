17e69c0f891492544fe27cdce4010510cdb8ab7b
IlyaViz <Bezzubik12345@i.ua>
2023-04-16 16:04:04 +0300

    add new unauthorized_required decorator

--- decorators.py
+++ decorators.py
@@ -5,3 +5,11 @@ def authorized_required(function):
         else:
             update.message.reply_text("You are not authorized")
     return wrapper
+
+def unauthorized_required(function):
+    def wrapper(update, context):
+        if context.user_data.get("authorized", False):
+            update.message.reply_text("You are already authorized")
+        else:
+            function(update, context)
+    return wrapper
