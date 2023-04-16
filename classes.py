333f519d420578159552598d845d4334102a9903
IlyaViz <110900698+IlyaViz@users.noreply.github.com>
2023-04-16 18:18:33 +0300

    descriptions changes

--- classes.py
+++ classes.py
@@ -22,7 +22,7 @@ class DbStatuses(enum.Enum):
         descriptions = {DbStatuses.success:"Success",
                         DbStatuses.no_data:"This data wasn't found. Check if input data exists and has something inside",
                         DbStatuses.user_already_created:"Seems that user was already created",
-                        DbStatuses.inserting_error:"Something went wrong, maybe already exists",
+                        DbStatuses.inserting_error:"Something went wrong. Common reasons: word is too long or something(word, group) already exists",
                         DbStatuses.group_not_exist:"There is no such group",
                         DbStatuses.blank_list:"The result is empty"}
         return descriptions[self]
